6e82e015a5bf91260be0fa129bbf4aa853f46676
SalcedoManuel <m.salcedoa.2016@alumnos.urjc.es>
2023-06-25 22:00:59 +0200

    css

--- script/resultados/Treatment of Quiz Questions/SelectOptionReadFile.js
+++ script/resultados/Treatment of Quiz Questions/SelectOptionReadFile.js
@@ -1,9 +1,10 @@
 
-function SelectOption2Show(option) {
-    console.info(document.getElementById(option).style.display)
+function SelectOption2Show(option,number) {
     if (document.getElementById(option).style.display == "block") {
         document.getElementById(option).style.display = "none";
+        document.getElementById("button_select_quiz"+number).style = "background-color: rgb(235,235,235);color: #000000;"
     }else{
         document.getElementById(option).style.display = "block";
+        document.getElementById("button_select_quiz"+number).style = "background-color: rgb(34,34,34); color: #ffff;"
     }
 }
